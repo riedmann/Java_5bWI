dfadcb7a57b0563975fbdce5fb8e213947e360e8
andreas riedmann <andreas@riedmann.rocks>
2023-11-14 13:54:29 +0100

    removed bin folder

--- Java/src/at/ran/patterns/observer/LightController.java
+++
@@ -1,9 +0,0 @@
-package at.ran.patterns.observer;
-public class LightController implements Observer{
-
-    @Override
-    public void inform(int temperature) {
-       System.out.println("INFO: " + temperature);
-    }
-    
-}
--- Java/src/at/ran/patterns/observer/Observer.java
+++
@@ -1,5 +0,0 @@
-package at.ran.patterns.observer;
-
-public interface Observer {
-    public void inform(int temperature);
-}
